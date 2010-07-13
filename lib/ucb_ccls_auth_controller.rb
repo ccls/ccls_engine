aaa0695b93a4244846d50a95670aae7f8c6ba333
Jake <github@jake.otherinbox.com>
2010-07-13 11:39:42 -0700

    Fixed application controller methods which are questionable.

--- lib/ucb_ccls_auth_controller.rb
+++ lib/ucb_ccls_auth_controller.rb
@@ -1,31 +1,18 @@
 module UcbCclsAuthController
 
 	def self.included(base)
-#		base.extend(ClassMethods)
-		base.send(:include, InstanceMethods)
-		base.class_eval do
 
-			before_filter :login_required
-
-			# Scrub sensitive parameters from your log
-			filter_parameter_logging :password
-
-			helper_method :current_user, :logged_in?
-
-		end
+		base.before_filter :login_required
+		base.helper_method :current_user, :logged_in?
+#		base.unloadable
 	end
 
+protected
 
-	module InstanceMethods
-
-	protected
-
-		def logged_in?
-			!current_user.nil?
-		end
-
+	def logged_in?
+		!current_user.nil?
 	end
 
 end
-#ActionController::Base.send(:include,UcbCclsAuthController)
+require 'application_controller'
 ApplicationController.send(:include,UcbCclsAuthController)
