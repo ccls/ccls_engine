0b6b6bc2259df551070388f1e5093b394a3ba4aa
Jake <github@jake.otherinbox.com>
2010-07-14 19:19:43 -0700

    Deving tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+ENV["RAILS_ENV"] = "test"
+require 'test/unit'			#	NEED THIS OR THE TESTS DON'T ACTUALLY EXECUTE
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
@@ -11,5 +13,17 @@ require 'action_mailer'
 
 #require File.dirname(__FILE__) + '/config/boot'
 require File.dirname(__FILE__) + '/config/environment'
+require 'test_help'
+
+require 'factories'
 
 
+def setup_db
+	ActiveRecord::Migrator.migrate("db/migrate/",nil)
+end
+
+def teardown_db
+	ActiveRecord::Migrator.migrate("db/migrate/",0)
+end
+
+setup_db()
