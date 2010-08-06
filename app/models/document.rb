b5b615f6a1d51b390d846b1681e778ef9dc5d63b
Jake <github@jake.otherinbox.com>
2010-08-05 17:37:28 -0700

    Beginning to implement S3 for private documents. [#8]

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,5 @@
+require 'hmac-sha1'
+require 'base64'
 class Document < ActiveRecord::Base
 	belongs_to :owner, :class_name => 'User'
 #	has_and_belongs_to_many :users
@@ -18,4 +20,50 @@ class Document < ActiveRecord::Base
 	def nullify_blank_document_file_name
 		self.document_file_name = nil if document_file_name.blank?
 	end
+
+#
+#	http://docs.amazonwebservices.com/AmazonS3/2006-03-01/index.html?RESTAuthentication.html
+#
+#Signature = URL-Encode( Base64( HMAC-SHA1( YourSecretAccessKeyID, UTF-8-Encoding-Of( StringToSign ) ) ) );
+#
+#StringToSign = HTTP-VERB + "\n" +
+#    Content-MD5 + "\n" +
+#    Content-Type + "\n" +
+#    Expires + "\n" +
+#    CanonicalizedAmzHeaders +
+#    CanonicalizedResource;    
+	def string_to_sign
+		"GET" + "\n" +
+		"\n" +
+		"\n" +
+		Chronic.parse('tomorrow').to_i.to_s + "\n" +
+		"" +
+		"/clic/documents/10/bethematch.gif"
+	end
+
+	def signature
+		s3 = YAML::load(IO.read('config/s3.yml'))
+		secret = s3['secret_access_key']
+#URI.encode(Base64.encode64s(HMAC::SHA1.digest(secret,string_to_sign)))
+#CGI.escape(Base64.encode64s(HMAC::SHA1.digest(secret,string_to_sign)))
+		#	URI.encode does NOT encode + or = which doesn't
+		#	work for Amazon's S3.  CGI.escape does.
+		#	http://redmine.ruby-lang.org/issues/show/1680
+		CGI.escape(Base64.encode64s(HMAC::SHA1.digest(secret,string_to_sign)))
+	end
+
+	def s3_url
+		s3 = YAML::load(IO.read('config/s3.yml'))
+		access_key_id = s3['access_key_id']
+		"https://s3.amazonaws.com/clic/documents/10/bethematch.gif?" +
+			"AWSAccessKeyId=#{access_key_id}&" +
+			"Signature=#{signature}&" +
+			"Expires=#{Chronic.parse('tomorrow').to_i}"
+	end
+
+#	Chronic.parse('tomorrow') is always tomorrow at noon
+#	so the time between hashing and requesting won't cause
+#	a problem unless the request is made at exactly the time
+#	that tomorrow becomes today as far as ruby is concerned.
+
 end
