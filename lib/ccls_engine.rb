a740961081eeabdec0d1f918f60fe0f947482b70
Jake <github@jake.otherinbox.com>
2010-10-18 16:42:17 -0700

    Added permissive and resourceful controller helpers.

--- lib/ccls_engine.rb
+++ lib/ccls_engine.rb
@@ -1,34 +1,3 @@
-
-#
-#	None of this seems necessary as I think that this
-#	gems loads them automagically due to the specs.
-#
-##config.gem 'i18n', :version => '=0.3.7'
-#
-#config.gem 'jrails'
-#
-#config.gem 'jakewendt-rails_helpers',
-# :lib    => 'rails_helpers', 
-# :source => 'http://rubygems.org'
-#
-#config.gem 'jakewendt-ruby_extension',
-# :lib    => 'ruby_extension', 
-# :source => 'http://rubygems.org'
-#
-#config.gem "chronic"
-#
-#config.gem "ruby-hmac", :lib => "ruby_hmac"
-#
-#config.gem "aws-s3", :lib => "aws/s3"
-#
-#config.gem 'ssl_requirement'
-#
-#config.gem 'paperclip'
-#
-#config.gem 'ryanb-acts-as-list', 
-# :lib => 'acts_as_list', 
-# :source => 'http://gems.github.com'
-
 require 'ruby_extension'
 require 'rails_helpers'
 require 'gravatar'
@@ -51,6 +20,8 @@ require 'ccls_engine/user_model'
 require 'ccls_engine/authorization'
 require 'ccls_engine/helper'
 require 'ccls_engine/controller'
+require 'ccls_engine/resourceful_controller'
+require 'ccls_engine/permissive_controller'
 require 'ccls_engine/redcloth/formatters/html'
 
 if !defined?(RAILS_ENV) || RAILS_ENV == 'test'
