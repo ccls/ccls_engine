22bb939a6e82179b2010400a013d20d22721503d
Jake <github@jake.otherinbox.com>
2010-11-24 12:56:35 -0800

    Re-added ccls_engine require 'user'.

--- generators/ccls_engine/templates/initializer.rb
+++ generators/ccls_engine/templates/initializer.rb
@@ -5,15 +5,19 @@
 #	a User is yet, which causes lots of ...
 #	NoMethodError (undefined method `find_create_and_update_by_uid' for nil:NilClass):
 #	so ...
-#require 'user' unless defined?(User)
-if RAILS_ENV == 'development' &&
-	!Gem.source_index.find_name('jakewendt-ccls_engine').empty?
-#if Gem.searcher.find('ccls_engine') && RAILS_ENV == 'development'
-require 'ccls_engine'	#	without this, rake has problems ...
-#undefined local variable or method `ucb_authenticated' for #<Class:0x1059408d0>
+unless Gem.source_index.find_name('jakewendt-ccls_engine').empty?
 require '<%=class_name.underscore-%>' unless defined?(<%=class_name-%>)
-#require 'role' unless defined?(Role)
 end
+
+#require 'user' unless defined?(User)
+#if RAILS_ENV == 'development' &&
+#	!Gem.source_index.find_name('jakewendt-ccls_engine').empty?
+#if Gem.searcher.find('ccls_engine') && RAILS_ENV == 'development'
+#require 'ccls_engine'	#	without this, rake has problems ...
+#undefined local variable or method `ucb_authenticated' for #<Class:0x1059408d0>
+#require '<%=class_name.underscore-%>' unless defined?(<%=class_name-%>)
+#require 'role' unless defined?(Role)
+#end
 #	Actually, this is probably only needed in development,
 #	but putting it in environments/development.rb doesn't
 #	work right, even in an after_initialize.
