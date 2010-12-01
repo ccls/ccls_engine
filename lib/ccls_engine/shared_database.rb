45dd2b371b4a398dfae0cc618dc7d148cac00b45
Jake <github@jake.otherinbox.com>
2010-12-01 14:40:08 -0800

    Prepping for multiple databases.

--- lib/ccls_engine/shared_database.rb
+++ lib/ccls_engine/shared_database.rb
@@ -1,49 +1,59 @@
 require 'active_record'
 require 'use_db'
 
-module UseDbTestExtension
-	def self.included(base)
-		unless base.methods.include?('my_other_databases') 
-			base.extend(ClassMethods)
-			base.class_eval do
-				class << self
-					alias :other_databases :my_other_databases
-				end
-			end
-		end
-	end
-	module ClassMethods
-		#	In preparation for keeping the use_db.yml DRY and in the gem
-		def my_other_databases
-#		def other_databases
-			YAML.load(
-				File.read( 
-					File.join(
-						File.dirname(__FILE__),'..','..','config','shared_use_db.yml'
-			))).values.collect(&:symbolize_keys!)
-		end
-	end
-end
-UseDbTest.send(:include,UseDbTestExtension)
+#module UseDbTestExtension
+#	def self.included(base)
+#		unless base.methods.include?('my_other_databases') 
+#			base.extend(ClassMethods)
+#			base.class_eval do
+#				class << self
+#					alias :other_databases :my_other_databases
+#				end
+#			end
+#		end
+#	end
+#	module ClassMethods
+#		#	In preparation for keeping the use_db.yml DRY and in the gem
+#		def my_other_databases
+##		def other_databases
+#			YAML.load(
+#				File.read( 
+#					File.join(
+#						File.dirname(__FILE__),'..','..','config','shared_use_db.yml'
+#			))).values.collect(&:symbolize_keys!)
+#		end
+#	end
+#end
+#UseDbTest.send(:include,UseDbTestExtension)
 
-module UseDbPluginExtension
-	def self.included(base)
-		unless base.methods.include?('get_use_db_conn_spec_without_ccls_read')
-			base.send(:include,ModuleMethods)
-			base.alias_method_chain :get_use_db_conn_spec, :ccls_read
-		end
+#module UseDbPluginExtension
+#	def self.included(base)
+#		unless base.methods.include?('get_use_db_conn_spec_without_ccls_read')
+#			base.send(:include,ModuleMethods)
+#			base.alias_method_chain :get_use_db_conn_spec, :ccls_read
+#		end
+#	end
+#	module ModuleMethods
+#		def get_use_db_conn_spec_with_ccls_read(options)
+#			if ActiveRecord::Base.configurations.empty?
+#				#	for rake tasks
+#				ActiveRecord::Base.configurations.update(YAML::load(ERB.new(IO.read(
+#					File.join( Rails.root,'config','database.yml'))).result))
+#			end
+#			ActiveRecord::Base.configurations.update(YAML::load(ERB.new(IO.read(
+#				File.join( Rails.root,'config','shared_database.yml'))).result))
+#			get_use_db_conn_spec_without_ccls_read(options)
+#		end
+#	end
+#end
+#UseDbPlugin.send(:include,UseDbPluginExtension)
+
+USE_DB_CONFIG  =  File.join( Rails.root,'config','shared_use_db.yml')
+OTHER_DB_FILES = [File.join( Rails.root,'config','shared_database.yml')]
+
+class ActiveSupport::TestCase
+	unless defined?(CLONED_DB_FOR_CCLS_TEST)
+		UseDbTest.prepare_test_db(:prefix => "shared_")
+		CLONED_DB_FOR_CCLS_TEST = true
 	end
-	module ModuleMethods
-		def get_use_db_conn_spec_with_ccls_read(options)
-			if ActiveRecord::Base.configurations.empty?
-				#	for rake tasks
-				ActiveRecord::Base.configurations.update(YAML::load(ERB.new(IO.read(
-					File.join( Rails.root,'config','database.yml'))).result))
-			end
-			ActiveRecord::Base.configurations.update(YAML::load(ERB.new(IO.read(
-				File.join( Rails.root,'config','shared_database.yml'))).result))
-			get_use_db_conn_spec_without_ccls_read(options)
-		end
-	end
-end
-UseDbPlugin.send(:include,UseDbPluginExtension)
+end if Rails.env == 'test'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,10 +5,6 @@ require 'test_help'
 $LOAD_PATH.unshift File.dirname(__FILE__) # NEEDED for rake test:coverage
 
 class ActiveSupport::TestCase
-	unless defined?(CLONED_SEC_DB_FOR_TEST)
-		UseDbTest.prepare_test_db(:prefix => "shared_")
-		CLONED_SEC_DB_FOR_TEST = true
-	end
 	self.use_transactional_fixtures = true
 	self.use_instantiated_fixtures  = false
 	fixtures :all
