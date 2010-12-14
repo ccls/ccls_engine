d793433e857f3687c0c156d13b24218c00990c17
Jake <github@jake.otherinbox.com>
2010-12-14 10:11:49 -0800

    trying to unmuck failed commit

--- db/migrate/20101214175931_add_future_use_prohibited_to_samples.rb
+++
@@ -1,9 +0,0 @@
-class AddFutureUseProhibitedToSamples < SharedMigration
-	def self.up
-		add_column :samples, :future_use_prohibited, :boolean, :default => false, :null => false
-	end
-
-	def self.down
-		remove_column :samples, :future_use_prohibited
-	end
-end
