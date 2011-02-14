b76f459a028c34de0cdc2c4f3643e06dd284cbf5
Jake <github@jakewendt.com>
2011-02-14 15:31:19 -0800

    Version bump to 3.2.0

--- lib/ccls_engine/active_record_extension.rb
+++
@@ -1,7 +0,0 @@
-module ActiveRecordExtension; end
-#$LOAD_PATH.unshift File.dirname(__FILE__)
-#$:.unshift File.dirname(__FILE__)
-$: << File.dirname(__FILE__)
-require "active_record_extension/base"
-require "active_record_extension/error"
-require "active_record_extension/errors"
