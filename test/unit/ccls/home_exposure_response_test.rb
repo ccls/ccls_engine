9a6295db53aa9546ae791fb4e290f61e6a1cfb35
Jake <github@jakewendt.com>
2011-05-06 15:13:45 -0700

    Added tests for all home exposure response fields.

--- test/unit/ccls/home_exposure_response_test.rb
+++ test/unit/ccls/home_exposure_response_test.rb
@@ -5,14 +5,13 @@ class Ccls::HomeExposureResponseTest < ActiveSupport::TestCase
 	assert_should_create_default_object
 	assert_should_initially_belong_to(:subject)
 	assert_should_require_unique_attribute(:study_subject_id)
-	assert_should_not_require_attributes( :consent_read_over_phone )
-	assert_should_not_require_attributes( :respondent_requested_new_consent )
-	assert_should_not_require_attributes( :consent_reviewed_with_respondent )
 
 	test "should return array of field_names" do
 		field_names = HomeExposureResponse.field_names
 		assert field_names.is_a?(Array)
 		assert field_names.length > 100
 	end
+
+	assert_should_not_require_attributes( *HomeExposureResponse.field_names )
 
 end
