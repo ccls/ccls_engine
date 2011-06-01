d4798d09ba1fc75c0c3975f03455e84cff622600
Jake <github@jakewendt.com>
2011-06-01 16:45:05 -0700

    Added a bit to autotest.rb

--- lib/ccls_engine/autotest.rb
+++ lib/ccls_engine/autotest.rb
@@ -18,6 +18,23 @@ class Autotest::Rails
 			) do |filename, _|
 			filename
 		end
+
+		##
+		#	This stops the ...
+		#
+		#	Unable to map class Ccls::IdentifierTest to a file
+		#	Unable to map class Ccls::SubjectTest to a file
+		#
+		#	By default autotest is expecting all the namespaces to end with Test
+		#		ie. CclsTest::IdentifierTest
+		#	Could have renamed the dir, I suppose.
+		#
+		Dir[File.join(File.dirname(__FILE__),'/../../test/unit/**/*rb')].each do |f|
+			if f =~ /test\/unit\/(ccls\/.*)\.rb/
+				self.extra_class_map[$1.camelcase] = File.expand_path(f)
+			end
+		end
+
 		run_without_ccls_engine
 	end
 	alias_method_chain :run, :ccls_engine
