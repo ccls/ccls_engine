708a24453f61d6498c826f3dedf448cb01cead68
Jake <github@jakewendt.com>
2011-06-01 10:22:49 -0700

    Added a basic test.

--- test/unit/ccls/childid_test.rb
+++ test/unit/ccls/childid_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class Ccls::ChildidTest < ActiveSupport::TestCase
+
+	test "should create and destroy" do
+		assert_difference("#{model_name}.next_id",1) {
+		assert_difference("#{model_name}.count",0) {
+			Childid.create!.destroy
+		} }
+	end
+
 end
--- test/unit/ccls/patid_test.rb
+++ test/unit/ccls/patid_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class Ccls::PatidTest < ActiveSupport::TestCase
+
+	test "should create and destroy" do
+		assert_difference("#{model_name}.next_id",1) {
+		assert_difference("#{model_name}.count",0) {
+			Patid.create!.destroy
+		} }
+	end
+
 end
