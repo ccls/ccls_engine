5ab4162b5311a42dfaf6a58f0bc66ba358dadf99
Jake <github@jakewendt.com>
2011-09-08 13:55:39 -0700

    Added skip_show_failure to calendar tests

--- test/functional/ccls/calendars_controller_test.rb
+++ test/functional/ccls/calendars_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class Ccls::CalendarsControllerTest < ActionController::TestCase
 	tests CalendarsController
 
-	ASSERT_ACCESS_OPTIONS = { :actions => [:show] }
+	ASSERT_ACCESS_OPTIONS = { :actions => [:show],
+		:skip_show_failure => true }
 
 	assert_access_with_login(    :logins => site_readers )
 	assert_no_access_with_login( :logins => non_site_readers )
