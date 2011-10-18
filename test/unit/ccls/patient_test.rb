a3530cb782910b2682ba493a14fb5457285f34cd
Jake <github@jakewendt.com>
2011-10-18 16:49:04 -0700

    Problems with new requirements.[#155][#156]

--- test/unit/ccls/patient_test.rb
+++ test/unit/ccls/patient_test.rb
@@ -2,18 +2,25 @@ require 'test_helper'
 
 class Ccls::PatientTest < ActiveSupport::TestCase
 
-#	assert_should_require :organization_id, :admit_date
-	assert_should_not_require :organization_id
-	assert_should_not_require :admit_date
 	assert_should_create_default_object
 	assert_should_initially_belong_to :study_subject
-	assert_should_initially_belong_to :organization
+#	assert_should_initially_belong_to :organization
+	assert_should_belong_to :organization
 	assert_should_protect( :study_subject_id )
 	assert_should_belong_to( :diagnosis )
-	assert_should_not_require_attributes :diagnosis_date
-	assert_should_not_require_attributes :diagnosis_id
-	assert_should_not_require_attributes :raf_zip
-	assert_should_not_require_attributes :raf_county
+
+	assert_should_require_attributes(
+		:organization_id,
+		:admit_date )
+
+	assert_should_not_require_attributes(
+		:organization_id,
+		:admit_date,
+		:diagnosis_date,
+		:diagnosis_id,
+		:raf_zip,
+		:raf_county )
+
 	assert_should_require_attribute_length( :raf_zip, :maximum => 10 )
 	assert_requires_complete_date :admit_date
 	assert_requires_complete_date :diagnosis_date
