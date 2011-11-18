1939b2d5d4db870367039e2669d2fb6844878327
Jake <github@jakewendt.com>
2011-11-18 14:24:45 -0800

    Imported calnet_authenticated stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,17 @@ ActionController::Routing::Routes.draw do |map|
 #		user.resources :roles, :only => [:update,:destroy]
 #	end
 
+
+#	from calnet_authenticated
+	map.logout 'logout', :controller => 'sessions', :action => 'destroy'
+	map.resources :users, :only => [:destroy,:show,:index],
+		:collection => { :menu => :get } do |user|
+		user.resources :roles, :only => [:update,:destroy]
+	end
+	map.resource :session, :only => [ :destroy ]
+
+
+
 #	map.logout 'logout', :controller => 'sessions', :action => 'destroy'
 #	map.resource :session, :only => [ :destroy ]
 
