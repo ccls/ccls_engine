7a4e86fbb813639a0377804b4cf229c1cd5f9866
Jake <github@jakewendt.com>
2012-02-02 14:04:03 -0800

    Prepping for updating from IcfMasterTracker.

--- app/models/icf_master_tracker.rb
+++ app/models/icf_master_tracker.rb
@@ -11,17 +11,66 @@ class IcfMasterTracker < ActiveRecordShared
 	def attach_study_subject
 		unless study_subject_id
 			identifier = Identifier.find_by_icf_master_id(self.Masterid)
-			if identifier and identifier.study_subject
-				self.study_subject = identifier.study_subject
+			if identifier and !identifier.study_subject_id.blank?
+				self.study_subject_id = identifier.study_subject_id
 			end
 		end
 	end
 
 	def update_models
+#
+#	"Masterid","Motherid","Record_Owner","Datereceived","Lastatt","Lastdisp",
+#	"Currphone","Vacauthrecd","Recollect","Needpreincentive","Active_Phone",
+#	"Recordsentformatching","Recordreceivedfrommatching","Sentpreincentive",
+#	"Releasedtocati","Confirmedcaticontact","Refused","Deceasednotification",
+#	"Eligible","Confirmationpacketsent","Catiprotocolexhausted",
+#	"Newphonenumreleasedtocati","Pleanotificationsent",
+#	"Casereturnedtoberkeleyfornewinf","Casereturnedfromberkeley","Caticomplete",
+#	"Kitmothersent","Kitinfantsent","Kitchildsent","Kitadolescentsent",
+#	"Kitmotherrefusedcode","Kitchildrefusedcode","Noresponsetoplea",
+#	"Responsereceivedfromplea","Senttoinpersonfollowup","Kitmotherrecd",
+#	"Kitchildrecvd","Thankyousent","Physrequestsent","Physresponsereceived"
+#
+#	Most of the columns are dates.
+#
 #		if study_subject_id and dirty
+#		Consider Record_Owner field?
+#		Which project?
 #			add operational event with differences to study subject
 #			update models
 #		end
+
+		if study_subject_id and self.changed?
+#			puts
+#			puts "Tracker has subject and has changed so begin updating"
+#			puts self.changes
+#			which changes matter?
+		else
+#			puts
+#			puts "Tracker has no subject so skipping updating"
+		end
 	end
 
 end
+
+__END__
+
+
+changed?() public
+
+Returns true if any attribute have unsaved changes, false otherwise.
+
+person.changed? # => false
+person.name = 'bob'
+person.changed? # => true
+
+
+changes() public
+
+Map of changed attrs => [original value, new value].
+
+person.changes # => {}
+person.name = 'bob'
+person.changes # => { 'name' => ['bill', 'bob'] }
+
+
--- app/models/icf_master_tracker_update.rb
+++ app/models/icf_master_tracker_update.rb
@@ -16,45 +16,29 @@ class IcfMasterTrackerUpdate < ActiveRecordShared
 
 #	"Masterid","Motherid","Record_Owner","Datereceived","Lastatt","Lastdisp","Currphone","Vacauthrecd","Recollect","Needpreincentive","Active_Phone","Recordsentformatching","Recordreceivedfrommatching","Sentpreincentive","Releasedtocati","Confirmedcaticontact","Refused","Deceasednotification","Eligible","Confirmationpacketsent","Catiprotocolexhausted","Newphonenumreleasedtocati","Pleanotificationsent","Casereturnedtoberkeleyfornewinf","Casereturnedfromberkeley","Caticomplete","Kitmothersent","Kitinfantsent","Kitchildsent","Kitadolescentsent","Kitmotherrefusedcode","Kitchildrefusedcode","Noresponsetoplea","Responsereceivedfromplea","Senttoinpersonfollowup","Kitmotherrecd","Kitchildrecvd","Thankyousent","Physrequestsent","Physresponsereceived"
 
-#				identifier = Identifier.find_by_icf_master_id(line['Masterid'])
-#				if identifier
-#					results.push(identifier.study_subject)
-#				else
-#					results.push("Could not find identifier with masterid #{line['Masterid']}")
-#					next
-#				end
-
-
-#
-#	I think that I am going to create a table and model IcfMasterTracker
-#	which will basically be identical to this file.  I will find or create
-#	by masterid and then update all of the columns.
 #
 #	The IcfMasterTracker will include an after_save or something that will
 #	determine what has changed and update appropriately.  It may also
-#	Create OperationalEvents to document the data changes.  As it is
-#	theoretically possible that the masterid does not exist in the identifiers
+#	create OperationalEvents to document the data changes.  As it is
+#	theoretically possible that the Masterid does not exist in the identifiers
 #	table, perhaps add a successfully_updated_models flag which could
 #	be used?
 #
-#	line.to_hash will return a nice string keyed hash for updating.
-#
-	icf_master_tracker = IcfMasterTracker.find_or_create_by_Masterid(line['Masterid'])
-#	NO BANG? Don't want to raise any errors.
-	successfully_updated = icf_master_tracker.update_attributes(line.to_hash)
+				icf_master_tracker = IcfMasterTracker.find_or_create_by_Masterid(line['Masterid'])
+				#	NO BANG. Don't want to raise any errors.
+				successfully_updated = icf_master_tracker.update_attributes(line.to_hash)
 #
 #	errors = icf_master_tracker.errors.full_messages.to_sentence
-#	these won't be validation errors as there shouldn't be any
-#	perhaps no column by that name errors?
+#	These won't be validation errors as there shouldn't be any.
+#	Perhaps "no column by that name" errors if csv file changes?
 #
-#	add successfully_updated value
+#	Add successfully_updated value?
 #		icf_master_tracker.update_attribute(:sucessfully_updated, successfully_updated)
 #	will the above include the line's attributes?
 #
-#	add update_errors column
+#	Add update_errors column?
 #		icf_master_tracker.update_attribute(:update_errors, errors)
 #
-#	perhaps add a study_subject_id field as well?
 
 				results.push(icf_master_tracker)
 
