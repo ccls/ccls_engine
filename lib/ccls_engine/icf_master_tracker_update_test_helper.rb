db1a5f1b1accf05618b8a16545980d75e2d40047
Jake <github@jakewendt.com>
2012-02-01 21:29:17 -0800

    Updated test helpers.

--- lib/ccls_engine/icf_master_tracker_update_test_helper.rb
+++ lib/ccls_engine/icf_master_tracker_update_test_helper.rb
@@ -40,12 +40,9 @@ module Ccls::IcfMasterTrackerUpdateTestHelper
 		study_subject
 	end
 
-#	def csv_file_header
-#		%{"Masterid","Motherid","Record_Owner","Datereceived","Lastatt","Lastdisp","Currphone","Vacauthrecd","Recollect","Needpreincentive","Active_Phone","Recordsentformatching","Recordreceivedfrommatching","Sentpreincentive","Releasedtocati","Confirmedcaticontact","Refused","Deceasednotification","Eligible","Confirmationpacketsent","Catiprotocolexhausted","Newphonenumreleasedtocati","Pleanotificationsent","Casereturnedtoberkeleyfornewinf","Casereturnedfromberkeley","Caticomplete","Kitmothersent","Kitinfantsent","Kitchildsent","Kitadolescentsent","Kitmotherrefusedcode","Kitchildrefusedcode","Noresponsetoplea","Responsereceivedfromplea","Senttoinpersonfollowup","Kitmotherrecd","Kitchildrecvd","Thankyousent","Physrequestsent","Physresponsereceived"}
-#	end
-
 	def csv_file_header_array
-		["Masterid","Motherid","Record_Owner","Datereceived","Lastatt","Lastdisp","Currphone","Vacauthrecd","Recollect","Needpreincentive","Active_Phone","Recordsentformatching","Recordreceivedfrommatching","Sentpreincentive","Releasedtocati","Confirmedcaticontact","Refused","Deceasednotification","Eligible","Confirmationpacketsent","Catiprotocolexhausted","Newphonenumreleasedtocati","Pleanotificationsent","Casereturnedtoberkeleyfornewinf","Casereturnedfromberkeley","Caticomplete","Kitmothersent","Kitinfantsent","Kitchildsent","Kitadolescentsent","Kitmotherrefusedcode","Kitchildrefusedcode","Noresponsetoplea","Responsereceivedfromplea","Senttoinpersonfollowup","Kitmotherrecd","Kitchildrecvd","Thankyousent","Physrequestsent","Physresponsereceived"]
+#		["Masterid","Motherid","Record_Owner","Datereceived","Lastatt","Lastdisp","Currphone","Vacauthrecd","Recollect","Needpreincentive","Active_Phone","Recordsentformatching","Recordreceivedfrommatching","Sentpreincentive","Releasedtocati","Confirmedcaticontact","Refused","Deceasednotification","Eligible","Confirmationpacketsent","Catiprotocolexhausted","Newphonenumreleasedtocati","Pleanotificationsent","Casereturnedtoberkeleyfornewinf","Casereturnedfromberkeley","Caticomplete","Kitmothersent","Kitinfantsent","Kitchildsent","Kitadolescentsent","Kitmotherrefusedcode","Kitchildrefusedcode","Noresponsetoplea","Responsereceivedfromplea","Senttoinpersonfollowup","Kitmotherrecd","Kitchildrecvd","Thankyousent","Physrequestsent","Physresponsereceived"]
+		%w{Masterid Motherid Record_Owner Datereceived Lastatt Lastdisp Currphone Vacauthrecd Recollect Needpreincentive Active_Phone Recordsentformatching Recordreceivedfrommatching Sentpreincentive Releasedtocati Confirmedcaticontact Refused Deceasednotification Eligible Confirmationpacketsent Catiprotocolexhausted Newphonenumreleasedtocati Pleanotificationsent Casereturnedtoberkeleyfornewinf Casereturnedfromberkeley Caticomplete Kitmothersent Kitinfantsent Kitchildsent Kitadolescentsent Kitmotherrefusedcode Kitchildrefusedcode Noresponsetoplea Responsereceivedfromplea Senttoinpersonfollowup Kitmotherrecd Kitchildrecvd Thankyousent Physrequestsent Physresponsereceived}
 	end
 
 	def csv_file_header
@@ -60,45 +57,46 @@ module Ccls::IcfMasterTrackerUpdateTestHelper
 	def study_subject_hash
 		{
 			"Masterid" => "1234FAKE",
-			"Motherid" => "4567FAKE",
-			"Record_Owner" => "ICF",
-			"Datereceived" => "9/9/2011",
-			"Lastatt" => "12/17/2011",
-			"Lastdisp" => "113",
-			"Currphone" => "2 of 2",
-			"Vacauthrecd" => nil,
-			"Recollect" => nil,
-			"Needpreincentive" => "9/17/11 9:29 AM",
-			"Active_Phone" => nil,
-			"Recordsentformatching" => "9/16/2011",
-			"Recordreceivedfrommatching" => "9/16/2011",
-			"Sentpreincentive" => "9/17/2011",
-			"Releasedtocati" => "9/17/2011",
-			"Confirmedcaticontact" => "9/28/2011",
-			"Refused" => "12/15/2011",
-			"Deceasednotification" => nil,
-			"Eligible" => nil,
-			"Confirmationpacketsent" => nil,
-			"Catiprotocolexhausted" => "12/17/2011",
-			"Newphonenumreleasedtocati" => "11/14/2011",
-			"Pleanotificationsent" => "11/14/2011",
-			"Casereturnedtoberkeleyfornewinf" => "12/19/2011",
-			"Casereturnedfromberkeley" => "12/22/2011",
-			"Caticomplete" => nil,
-			"Kitmothersent" => nil,
-			"Kitinfantsent" => nil,
-			"Kitchildsent" => nil,
-			"Kitadolescentsent" => nil,
-			"Kitmotherrefusedcode" => nil,
-			"Kitchildrefusedcode" => nil,
-			"Noresponsetoplea" => nil,
-			"Responsereceivedfromplea" => nil,
-			"Senttoinpersonfollowup" => nil,
-			"Kitmotherrecd" => nil,
-			"Kitchildrecvd" => nil,
-			"Thankyousent" => nil,
-			"Physrequestsent" => nil,
-			"Physresponsereceived" => nil
+			"Motherid" => "4567FAKE"
+
+#			"Record_Owner" => "ICF",
+#			"Datereceived" => "9/9/2011",
+#			"Lastatt" => "12/17/2011",
+#			"Lastdisp" => "113",
+#			"Currphone" => "2 of 2",
+#			"Vacauthrecd" => nil,
+#			"Recollect" => nil,
+#			"Needpreincentive" => "9/17/11 9:29 AM",
+#			"Active_Phone" => nil,
+#			"Recordsentformatching" => "9/16/2011",
+#			"Recordreceivedfrommatching" => "9/16/2011",
+#			"Sentpreincentive" => "9/17/2011",
+#			"Releasedtocati" => "9/17/2011",
+#			"Confirmedcaticontact" => "9/28/2011",
+#			"Refused" => "12/15/2011",
+#			"Deceasednotification" => nil,
+#			"Eligible" => nil,
+#			"Confirmationpacketsent" => nil,
+#			"Catiprotocolexhausted" => "12/17/2011",
+#			"Newphonenumreleasedtocati" => "11/14/2011",
+#			"Pleanotificationsent" => "11/14/2011",
+#			"Casereturnedtoberkeleyfornewinf" => "12/19/2011",
+#			"Casereturnedfromberkeley" => "12/22/2011",
+#			"Caticomplete" => nil,
+#			"Kitmothersent" => nil,
+#			"Kitinfantsent" => nil,
+#			"Kitchildsent" => nil,
+#			"Kitadolescentsent" => nil,
+#			"Kitmotherrefusedcode" => nil,
+#			"Kitchildrefusedcode" => nil,
+#			"Noresponsetoplea" => nil,
+#			"Responsereceivedfromplea" => nil,
+#			"Senttoinpersonfollowup" => nil,
+#			"Kitmotherrecd" => nil,
+#			"Kitchildrecvd" => nil,
+#			"Thankyousent" => nil,
+#			"Physrequestsent" => nil,
+#			"Physresponsereceived" => nil
 		}
 	end
 
