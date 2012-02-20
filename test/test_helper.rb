dc0e88e661192a9944609669b4f4e597b2ee962e
Jake <github@jakewendt.com>
2012-02-19 19:38:30 -0800

    No longer using controllers in gem so no need for turning https on

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,5 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
 class ActiveSupport::TestCase
-
 	fixtures :all
-
 end
-
-#class ActionController::TestCase
-#
-#	setup :turn_https_on
-#
-#end
