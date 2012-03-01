feefbdb3a2222879c13805fe9e2e81b75aa3e8a3
Jake <github@jakewendt.com>
2012-02-29 16:52:52 -0800

    Minor mods

--- generators/ccls_engine/ccls_engine_generator.rb
+++ generators/ccls_engine/ccls_engine_generator.rb
@@ -74,14 +74,14 @@ class CclsEngineGenerator < Rails::Generator::Base
 #			}
 
 			m.directory('test/fixtures')
-			Dir["#{dot}/templates/fixtures/*yml"].each{|file| 
+			Dir["#{dot}/templates/fixtures/*{yml,csv}"].each{|file| 
 				f = file.split('/').slice(-2,2).join('/')
 				m.file(f, "test/fixtures/#{File.basename(file)}")
 			}
-			Dir["#{dot}/templates/fixtures/*csv"].each{|file| 
-				f = file.split('/').slice(-2,2).join('/')
-				m.file(f, "test/fixtures/#{File.basename(file)}")
-			}
+#			Dir["#{dot}/templates/fixtures/*csv"].each{|file| 
+#				f = file.split('/').slice(-2,2).join('/')
+#				m.file(f, "test/fixtures/#{File.basename(file)}")
+#			}
 
 #			m.directory('test/unit/ccls')
 #			Dir["#{File.dirname(__FILE__)}/templates/unit/*rb"].each{|file| 
