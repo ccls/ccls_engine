0c730ad3cc6a65d53db0c893dc163875f8f98abf
Jake <github@jakewendt.com>
2012-03-08 16:07:20 -0800

    Looking into creating virtual attributes for datetime pieces.

--- lib/ccls_engine/active_record_extension/base.rb
+++ lib/ccls_engine/active_record_extension/base.rb
@@ -1,7 +1,7 @@
 module Ccls::ActiveRecordExtension::Base
 
 	def self.included(base)
-		base.send(:include, InstanceMethods)
+#		base.send(:include, InstanceMethods)
 		base.extend(ClassMethods)
 	end
 
@@ -11,8 +11,7 @@ module Ccls::ActiveRecordExtension::Base
 			options = {
 				:key   => :key,
 				:value => :description
-			}
-			options.update(args.extract_options!)
+			}.update(args.extract_options!)
 
 			class_eval do
 
@@ -24,17 +23,47 @@ module Ccls::ActiveRecordExtension::Base
 				# Treats the class a bit like a Hash and
 				# searches for a record with a matching key.
 				def self.[](key)
-# may need to deal with options[:key] here, but not needed yet
+					# may need to deal with options[:key] here, but not needed yet
 					find_by_key(key.to_s)
 				end
 
 			end	#	class_eval do
-		end
+		end	#	def acts_like_a_hash(*args)
+
+
+#		#
+#		#	Create virtual attribute accessors for datetime parts,
+#		#		year, month, day, hour, minute for use by 
+#		#		view selectors and model validations?
+#		#
+#		#	If this all works, may want to separate it out.
+#		#
+#		def datetime_accessors_for(*args)
+#			options = { 
+#			}.update(args.extract_options!)
+#			args.each do |field|
+#
+##				What if there is no 'field'?  Typo?
+#
+##				When to actually compile the 'field'
+#
+##				define instance variable "#{field}_year"
+#
+#				define_method "#{field}_year" do
+#					#	return the instance variable? or pull it from 'field'?
+#				end
+#
+#				define_method "#{field}_year=" do |new_year|
+#					#	just set the instance variable?
+#				end
+#
+#			end
+#		end	#	def datetime_accessors_for(*args)
 
 	end	#	module ClassMethods
 
-	module InstanceMethods
-	end	#	module InstanceMethods
+#	module InstanceMethods
+#	end	#	module InstanceMethods
 
 end	#	module Ccls::ActiveRecordExtension::Base
 ActiveRecord::Base.send(:include, 
